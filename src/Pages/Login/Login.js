414dc52b8c5efe98c0180d0ab5cb7703dfe794ef
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2023-08-19 16:38:43 +0530

    social-media

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -11,6 +11,10 @@ function Login() {
  
   const {isFetching,user,isError,dispatch}=useContext(AuthContext)
 
+  useEffect(()=>{
+    email.current.focus()
+  },[])
+
 
   const handleClick=(e)=>{
     e.preventDefault()
@@ -22,13 +26,18 @@ function Login() {
 
   return (
     <div className='loginContainer'>
-       <form onSubmit={handleClick}>
+       <div className='loginLeft'>
+        <img className='loginLeftImg' src="https://upload.wikimedia.org/wikipedia/en/thumb/0/04/Facebook_f_logo_%282021%29.svg/800px-Facebook_f_logo_%282021%29.svg.png" alt='loginLeftImg' />
+        <h4 className='loginLeftName'>facebook</h4>
+        <h6 className='loginLeftDes'>Facebook helps you connect and share with the people in  your  life</h6>
+       </div>
+       <form className='loginRightContainer' onSubmit={handleClick}>
         <label  className='loginText'>Email</label><br/>
         <input className='loginInput' type='email'  ref={email}  /><br/>
         <label className='loginText'>Password</label><br/>
         <input className='loginInput' type='password' ref={password} /><br/>
         <input className='loginButton' type='submit' value="Log in"/><br/>
-        <span className='forgotPassword'>Forgot Password ? </span><br/>
+        <span style={{visibility:"hidden"}} className='forgotPassword'>Forgot Password ? </span><br/>
         <Link to="/register" className='signupButton'>Sign Up</Link>
        </form>
     </div>
--- src/apicalls.js
+++ src/apicalls.js
@@ -5,7 +5,7 @@ import axios from "axios"
 export const loginCall=async(userCrendentials,dispatch)=>{
     dispatch({type:"login_start"})
     try {
-        const res=await axios.post("http://localhost:8000/api/auth/login",userCrendentials)
+        const res=await axios.post("https://social-media-backend-f9xi.onrender.com/api/auth/login",userCrendentials)
         dispatch({type:"success",payload:res.data})
     } catch (error) {
         dispatch({type:"failure",payload:error})
