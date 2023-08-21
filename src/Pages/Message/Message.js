99f94c2af886164bf9431076be80801c03c8379d
arundhi04101996@gmail.com <arundhi04101996@gmail.com>
2023-08-21 10:10:28 +0530

    social-media

--- src/Pages/Message/Message.js
+++ src/Pages/Message/Message.js
@@ -20,7 +20,7 @@ function Message() {
   const socket=useRef()
 
   useEffect(()=>{
-    socket.current= io("https://social-media-socket-cvjt.onrender.com")
+    socket.current= io("ws://localhost:8900")
      socket.current?.on("getmessage",(data)=>{
        setArrivalMessage({
          senderId:data.senderId,
